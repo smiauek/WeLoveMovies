df2e81e381bdd1cac06a701e97c0309870305884
Piotr Smialkowski <smiaucius@gmail.com>
2022-02-02 11:54:15 -0600

    enabled cors for entire api

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const notFound = require("./errors/notFound");
@@ -9,7 +10,7 @@ const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
 
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
